f03babcf1d89fccb5e92048fedb3f7855ce918bf
FacundoAylan <facundogsb@gmail.com>
2024-11-13 21:21:15 -0300

    subiendo arreglos en el layout

--- app/layout.js
+++ app/layout.js
@@ -16,6 +16,12 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://next-portafolio-sigma.vercel.app/" />
+        <meta property="og:title" content="Portafolio de Facundo Aylan" />
+        <meta property="og:description" content="Portafolio personal en el cual demuestro mis habilidades." />
+        <meta property="og:image" content="/image/profile.png" />
+
       </head>
       <body className={`${inter.className} h-full`}>
         <header className="fixed top-0 left-0 w-full h-16 z-50">
